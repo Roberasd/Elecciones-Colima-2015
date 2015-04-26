2cd680c8c47761cdeb8bde9a68e07d0e92520dc9
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-04-26 11:29:04 -0500

    renombrando carpetas

---
+++ js/Candidatos.js
@@ -0,0 +1,15 @@
+var candidatos = null
+// candidatos(?, nombre candidato, frase, imagen, partido, twitter)
+function CargarCandidatos()
+{
+candidatos = [
+Candidato(0, 'José Ignacio Peralta', 'Candidato del PRI al gobierno de Colima', 'peralta.jpg', 0, '@nachoperaltacol'),
+Candidato(0, 'Jorge Luis Preciado', 'Candidato del PAN al gobierno de Colima', 'preciado.jpg', 1, '@JL_Preciado_'),
+Candidato(0, 'Martha Maria Zepeda del Tore', 'Candidato del PRD al gobierno de Colima', 'zepeda.jpg', 2, ''),
+Candidato(0, 'David Munro', 'Candidato del PT al gobierno de Colima', 'munro.jpg', 3, ''),
+Candidato(0, 'José Francisco Gallardo Rodríguez', 'Candidato de Morena al gobierno de Colima', 'gallardo.jpg', 4, ''),
+Candidato(0, 'Carlos Barbazán Martínez', 'Candidato del Partido Humanista al gobierno de Colima', 'barbazan.jpg', 5, ''),
+Candidato(0, 'Gerardo Galván Pinto', 'Candidato de Encuentro Social al gobierno de Colima', 'galvan.jpg', 6, ''),
+Candidato(0, 'Leoncio Alfonso Morán Sánchez', 'Candidato de Movimiento Ciudadano al gobierno de Colima', 'moran.jpg', 7, ''),
+];
+}
