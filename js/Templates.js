a65363d71c8f041350440154671a5cdcc10061b6
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-05-01 22:46:15 -0500

    compartir twitter texto

--- js/Templates.js
+++ js/Templates.js
@@ -64,7 +64,7 @@ function CompartirInicio(tipo)
 	if(tipo == 0)
 	  window.open('https://twitter.com/intent/tweet?'+
 	  'related=ytuquepropones&'+
-	  'text='+ '%23ytuquepropones las propuestas de todos los candidatos en Colima ytuquepropones.com', 'tweet', 'width=900,height=300,menubar=no,status=no,titlebar=no,top=200,left='+(screen.width-900)/2);
+	  'text='+ '%23ytuquepropones las propuestas de los candidatos en Colima ytuquepropones.com', 'tweet', 'width=900,height=300,menubar=no,status=no,titlebar=no,top=200,left='+(screen.width-900)/2);
 	else
 	  window.open('http://www.facebook.com/dialog/feed?app_id=1641263312770943' +
         '&link=http://ytuquepropones.com' +
